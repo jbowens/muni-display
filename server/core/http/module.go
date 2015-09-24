1cff8fd5efe08656ef8f8b7654b60b564b4fa126
Jackson Owens <jackson@squareup.com>
2015-09-23 17:56:09 -0700

    Use config file for bind address

--- server/core/http/module.go
+++ server/core/http/module.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/jbowens/muni/server/core/config"
 	"github.com/jbowens/muni/server/core/predictions"
 	"github.com/octavore/naga/service"
 )
 
-const (
-	bindAddress = "localhost:8080"
-)
-
 // Module implements naga/service.Module and encapsulates the MUNI http server.
 type Module struct {
+	Config      *config.Module
 	Predictions *predictions.Module
+	config      httpConfig
 	mux         *http.ServeMux
+}
+
+type httpConfig struct {
+	BindAddress string `json:"bind_address"`
 }
 
 func (m *Module) Init(c *service.Config) {
@@ -24,16 +27,20 @@ func (m *Module) Init(c *service.Config) {
 }
 
 func (m *Module) setup() error {
+	if err := m.Config.Load("config.json", &m.config); err != nil {
+		return err
+	}
+
 	m.mux = http.NewServeMux()
 	m.mux.HandleFunc("/predictions/", m.handlePredictions)
 	return nil
 }
 
 func (m *Module) start() {
-	if err := http.ListenAndServe(bindAddress, m); err != nil {
+	if err := http.ListenAndServe(m.config.BindAddress, m); err != nil {
 		panic(err)
 	}
-	fmt.Printf("Listening on %s...\n", bindAddress)
+	fmt.Printf("Listening on %s...\n", m.config.BindAddress)
 }
 
 func (m *Module) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
